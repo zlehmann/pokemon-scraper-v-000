303ce365d0ea13c4f8d57278578e5eda8c959de7
Zachary Lehmann <zlehmann@gmail.com>
2018-11-02 01:07:21 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,10 +3,10 @@ class Pokemon
   attr_accessor :id, :name, :type, :db
 
   def initialize(id:, name:, type:, db:)
-    @id = :id
-    @name = :name
-    @type = :type
-    @db = :db
+    @id = id
+    @name = name
+    @type = type
+    @db = db
   end
 
   def self.save(name, type, db)
@@ -17,6 +17,6 @@ class Pokemon
     #binding.pry
     result = db.execute("SELECT * FROM pokemon WHERE pokemon.id = id;")
     #binding.pry
-    Pokemon.new(id: result[0][0], name: result[0][1], type: result[0][2], db: db)
+    new_pokemon = Pokemon.new(id: result[0][0], name: result[0][1], type: result[0][2], db: db)
   end
 end
